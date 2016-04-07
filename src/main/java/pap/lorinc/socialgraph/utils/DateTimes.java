2a1edb6427d453eda7955ee2ceb74a9539e351cf
Pap Lőrinc <paplorinc@yahoo.com>
2016-04-07 21:08:24 +0300

    Changed Duration display to a single time unit, instead of two
    * extracted constants
    * made calculations lazy

--- src/main/java/pap/lorinc/socialgraph/utils/DateTimes.java
+++ src/main/java/pap/lorinc/socialgraph/utils/DateTimes.java
@@ -5,21 +5,24 @@ import javaslang.control.Option;
 import lombok.experimental.UtilityClass;
 
 import java.time.Duration;
+import java.util.function.Supplier;
 
 import static javaslang.control.Option.none;
 import static javaslang.control.Option.of;
 
 @UtilityClass
 public class DateTimes {
+    private static final int HOURS_PER_DAY = 24, MINUTES_PER_HOUR = 60, SECONDS_PER_MINUTE = 60, MILLIS_PER_SECOND = 1000;
+
     public static String durationToString(Duration duration) {
-        return Stream.of(toString(duration.toDays(), "day", "days"),
-                         toString(duration.toHours() % 24, "hour", "hours"),
-                         toString(duration.toMinutes() % 60, "minute", "minutes"),
-                         toString(duration.getSeconds() % 60, "second", "seconds"),
-                         toString(duration.toMillis() % 1000, "milli", "millis"))
-                     .flatMap(Option::toStream)
-                     .take(2)
-                     .mkString(", ");
+        return Stream.<Supplier<Option<String>>>of(() -> toString(duration.toDays(), "day", "days"),
+                                                   () -> toString(duration.toHours() % HOURS_PER_DAY, "hour", "hours"),
+                                                   () -> toString(duration.toMinutes() % MINUTES_PER_HOUR, "minute", "minutes"),
+                                                   () -> toString(duration.getSeconds() % SECONDS_PER_MINUTE, "second", "seconds"),
+                                                   () -> toString(duration.toMillis() % MILLIS_PER_SECOND, "milli", "millis"))
+                       .flatMap(Supplier::get)
+                       .take(1)
+                       .mkString(", "); // ... in case you take more than 1 time unit
     }
     private static Option<String> toString(long duration, String singular, String plural) {
         switch ((int) duration) {
--- src/test/groovy/pap/lorinc/socialgraph/utils/DateTimesTest.groovy
+++ src/test/groovy/pap/lorinc/socialgraph/utils/DateTimesTest.groovy
@@ -15,25 +15,25 @@ import static pap.lorinc.socialgraph.utils.DateTimes.durationToString
         where:  durationString || result
                 'PT0.001S'     || '1 milli' 
                 'PT0.012S'     || '12 millis' 
-                'PT1.123S'     || '1 second, 123 millis' 
-                'PT62.012S'    || '1 minute, 2 seconds' 
+                'PT1.123S'     || '1 second' 
+                'PT62.012S'    || '1 minute' 
                 
                 'PT1S'         || '1 second' 
-                'PT1.2S'       || '1 second, 200 millis' 
-                'PT2.1234S'    || '2 seconds, 123 millis' 
-                'PT82.1234S'   || '1 minute, 22 seconds' 
+                'PT1.2S'       || '1 second' 
+                'PT2.1234S'    || '2 seconds' 
+                'PT82.1234S'   || '1 minute' 
                 
-                'PT1M12S'      || '1 minute, 12 seconds' 
-                'PT126M2S'     || '2 hours, 6 minutes' 
+                'PT1M12S'      || '1 minute' 
+                'PT126M2S'     || '2 hours' 
 
-                'PT1H4M'       || '1 hour, 4 minutes' 
-                'PT1H4M2S'     || '1 hour, 4 minutes' 
-                'PT2H2S'       || '2 hours, 2 seconds' 
-                'PT26H2S'      || '1 day, 2 hours' 
+                'PT1H4M'       || '1 hour' 
+                'PT1H4M2S'     || '1 hour' 
+                'PT2H2S'       || '2 hours' 
+                'PT26H2S'      || '1 day' 
                 
-                'P1DT3H'       || '1 day, 3 hours' 
-                'P2DT3H4M'     || '2 days, 3 hours' 
-                'P2DT4M'       || '2 days, 4 minutes' 
+                'P1DT3H'       || '1 day' 
+                'P2DT3H4M'     || '2 days' 
+                'P2DT4M'       || '2 days' 
     }
     /*@formatter:on*/
 } 
