6bb076e1b471b1e6ed1d1f8a6a9c1efb493cfce6
Pap Lőrinc <paplorinc@yahoo.com>
2016-04-14 13:44:37 +0300

    Simplified `Option` usage

--- src/main/java/pap/lorinc/socialgraph/actions/factory/Matchers.java
+++ src/main/java/pap/lorinc/socialgraph/actions/factory/Matchers.java
@@ -6,13 +6,11 @@ import org.intellij.lang.annotations.RegExp;
 import java.util.regex.Matcher;
 
 import static java.util.regex.Pattern.compile;
-import static javaslang.control.Option.none;
-import static javaslang.control.Option.of;
 
 public class Matchers {
     public static Option<Matcher> matcher(String line, @RegExp String pattern) {
         Matcher matcher = compile(pattern).matcher(line.trim());
-        return (matcher.matches() && (matcher.groupCount() > 0)) ? of(matcher)
-                                                                 : none();
+        return Option.when(matcher.matches() && (matcher.groupCount() > 0),
+                           () -> matcher);
     }
 }
